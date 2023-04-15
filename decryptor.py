198b8864e369830d1ec072ce2c4af0f319b9431a
miku1001 <pauloted.22@gmail.com>
2023-04-15 23:47:58 +0800

    Character's dictionary

--- decryptor.py
+++ decryptor.py
@@ -9,6 +9,12 @@ print(("\033[34mDecrypt your text here!\033[0m").center(52," "))
 print("_" * 45)
 
 # Create a dictionary for characters
+dict_char = {'*': 'a',
+             '&': 'e',
+             '#': 'i',
+             '+': 'o',
+             '!': 'u'}
+    
 # Substitute the character given by the user based on dictionary
 # Enter the text that user wants to decrypt
 # Let the user continue the process or stop
