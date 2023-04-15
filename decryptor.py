654c3e808bf67b97cca62a6b6fb21d82f77edcc9
miku1001 <pauloted.22@gmail.com>
2023-04-15 23:30:00 +0800

    Pseudocodes

---
+++ decryptor.py
@@ -0,0 +1,8 @@
+#Ted Paulo A. Feranil
+
+# Import pyfiglet module
+# Create header
+# Create a dictionary for characters
+# Substitute the character given by the user based on dictionary
+# Enter the text that user wants to decrypt
+# Let the user continue the process or stop
