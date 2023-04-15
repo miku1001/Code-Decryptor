27f0be136b2d52af376e4c572d13bc319beff513
miku1001 <pauloted.22@gmail.com>
2023-04-15 23:44:31 +0800

    User welcome greeting

--- decryptor.py
+++ decryptor.py
@@ -1,5 +1,13 @@
 # Import pyfiglet module
+import pyfiglet
+
 # Create header
+name = input("What's your name? ")
+print(pyfiglet.figlet_format(f"Hello {name} ! Welcome !", font="Ogre"))
+print("=" * 45)
+print(("\033[34mDecrypt your text here!\033[0m").center(52," "))
+print("_" * 45)
+
 # Create a dictionary for characters
 # Substitute the character given by the user based on dictionary
 # Enter the text that user wants to decrypt
